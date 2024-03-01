124fdd07e995156d7604983a090e9b7b9ee09f56
anthonypoon12 <anthony@poons.us>
2024-02-29 19:29:25 -0500

    added creation functionality (doesnt work)

--- client/src/app/createsets/components/CreateSets.js
+++ client/src/app/createsets/components/CreateSets.js
@@ -5,12 +5,82 @@ import { useRouter } from "next/navigation";
 
 export default function CreateSets() {
 	const router = useRouter();
+	const [inputs, setInputs] = useState({
+		prompt: "",
+		content: "",
+	});
 
 	const [errorMessage, setErrorMessage] = useState("");
 
+	const handleInputChange = (e) => {
+		setInputs({
+			...inputs,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		if (
+			inputs.prompt.trim() === "" ||
+			inputs.content.trim() === ""
+		)
+			setErrorMessage("Please enter a prompt and content!");
+		const res = await fetch("/createsets/api", {
+			method: "POST",
+			mode: "cors", // no-cors, *cors, same-origin
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(inputs),
+		});
+		if (!res.ok) {
+			const resJson = await res.json();
+			setErrorMessage(resJson.message);
+		}
+		if (res.ok) {
+			router.push("/createsets");
+		}
+	};
+
 	return (
 		<div className={`w-100 card p-5`}>
-			<h3 className="mb-2 pb-2 mb-3 border-bottom">Create Sets</h3>
+		<h3 className="mb-2 pb-2 mb-3 border-bottom">Create Sets</h3>
+		<form className="w-100 m-auto" onSubmit={onSubmit}>
+			<div className="form-group mb-3">
+				<label htmlFor="prompt-input">Prompt:</label>
+				<input
+					type="text"
+					name="Prompt"
+					className="form-control"
+					id="prompt-input"
+					value={inputs.prompt}
+					required
+					onChange={handleInputChange}
+					placeholder="Enter Prompt"
+				/>
+			</div>
+			<div className="form-group mb-3">
+				<label htmlFor="content-input">Content:</label>
+				<input
+					type="text"
+					name="Content"
+					className="form-control"
+					id="content-input"
+					value={inputs.content}
+					required
+					onChange={handleInputChange}
+					placeholder="Enter content"
+				/>
+			</div>
+			{errorMessage !== "" && (
+				<div className="alert alert-danger">{errorMessage}</div>
+			)}
+
+			<button type="submit" className="btn btn-primary">
+				Sign Up
+			</button>
+		</form>
 		</div>
 	);
 }
