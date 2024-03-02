5152297b84c755cff6e3e094da1c64f0aa0858cd
ivanrao <ivanrao@outlook.com>
2024-03-02 17:12:44 -0500

    set up Set model

---
+++ api/models/set.js
@@ -0,0 +1,30 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+	class Set extends Model {}
+
+	Set.init(
+		{
+			Set_Id: {
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
+			},
+			Name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			Class_Id: {
+				type: DataTypes.INTEGER,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Sets",
+		}
+	);
+
+	Set.associate = (models) => {};
+
+	return Set;
+};
