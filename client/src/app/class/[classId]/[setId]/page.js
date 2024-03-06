e7cc72f51a2ec1d40538f201ef3f41fc064e4f4f
ivanrao <ivanrao@outlook.com>
2024-03-05 20:28:35 -0500

    added a page to view a class' sets

--- client/src/app/class/[classId]/[setId]/page.js
+++ client/src/app/class/[classId]/[setId]/page.js
@@ -6,5 +6,10 @@ export default function Index({ params }) {
 	const headersList = headers();
 	const user = JSON.parse(headersList.get("user"));
 
-	return <div>Sets for Class: {params.classId}</div>;
+	return (
+		<div>
+			ClassId: {params.classId} <br />
+			SetId: {params.setId}
+		</div>
+	);
 }
--- client/src/app/class/[classId]/page.js
+++ client/src/app/class/[classId]/page.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { headers } from "next/headers";
+import ClassSets from "./components/ClassSets";
 const apiUrl = process.env.API_URL;
 
 export default function Index({ params }) {
 	const headersList = headers();
 	const user = JSON.parse(headersList.get("user"));
+	const classId = params.classId;
 
 	return (
-		<div>
-			This is the page for a specifc class. This is class {params.classId}
-		</div>
+		<main>
+			<div className="p-3">
+				This is the page for a specifc class. This is class{" "}
+				{params.classId}
+				<ClassSets classId={classId} />
+			</div>
+		</main>
 	);
 }
