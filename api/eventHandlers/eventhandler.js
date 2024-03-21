dd523b7426b54f3e14eaeb04496efb76128055af
anthonypoon12 <anthony@poons.us>
2024-03-21 19:04:14 -0400

    allow users to fetch all socket ids in room

--- api/eventHandlers/eventhandler.js
+++ api/eventHandlers/eventhandler.js
@@ -1,4 +1,17 @@
 module.exports = (io, socket) => {
+	const fetchSocketsInRoom = async (roomId) => {
+		try {
+			const sockets = await io.in(roomId).fetchSockets();
+			// return JSON.parse(sockets);
+			let socketIds = sockets.map((socket) => socket.id);
+			// return JSON.parse(socketIds);
+			return socketIds.toString();
+		} catch (error) {
+			console.error(`Error fetching sockets in room ${roomId}:`, error);
+			throw error;
+		}
+	};
+
 	socket.on("disconnect", function () {
 		console.log("user disconnected");
 	});
@@ -12,5 +25,9 @@ module.exports = (io, socket) => {
 		io.to(roomId).emit("roomMessage", "A new client has joined!");
 	});
 
+	socket.on("fetchSocketsInRoom", (roomId) => {
+		fetchSocketsInRoom(roomId).then((sockets) =>
+			socket.emit("allSocketsInRoom", JSON.stringify(sockets))
+		);
 	});
 };
