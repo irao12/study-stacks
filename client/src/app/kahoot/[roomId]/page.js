d62cad32ec1c832966953dd38b208984e29c052a
anthonypoon12 <anthony@poons.us>
2024-03-21 19:03:00 -0400

    kahoot should only be accessed in its own room

--- client/src/app/kahoot/[roomId]/page.js
+++ client/src/app/kahoot/[roomId]/page.js
@@ -1,13 +1,15 @@
 import Kahoot from "./components/Kahoot";
-import { socketClient } from "../sockets";
+import { socketClient } from "../../sockets";
 
-export default function Index() {
+export default function Index({ params }) {
+	const roomId = params.roomId;
+
 	return (
 		<main id="kahoot-page">
 			<div
 				className={`w-100 d-flex justify-content-center align-items-center`}
 			>
-				<Kahoot />
+				<Kahoot roomId={roomId} />
 			</div>
 		</main>
 	);
