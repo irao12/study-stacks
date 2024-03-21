cb59c7e7a7939d04822161dd8031918ac73d5cdf
Youssef Elsuradi <youssefelsuradi@gmail.com>
2024-03-21 16:19:58 -0400

    added index.js for class

---
+++ api/migrations/20240321195447-create-classes-table.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('class', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Owner_Id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      Name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('class');
+  }
+};
