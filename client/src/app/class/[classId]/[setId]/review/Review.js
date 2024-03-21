608d4d499e1095be76f30138a0ffc5b7b967cdcd
Stefani Hermanto <sc.shher@gmail.com>
2024-03-20 22:03:12 -0400

    Added functionality to see both sides and to go to next/previous cards

--- client/src/app/class/[classId]/[setId]/review/Review.js
+++ client/src/app/class/[classId]/[setId]/review/Review.js
@@ -1,8 +1,10 @@
+"use client"
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./review.module.css";
 import Icon from "@mdi/react";
-import { mdiArrowLeftCircleOutline, mdiArrowRightCircleOutline, mdiShuffle } from "@mdi/js";
+import { mdiDotsHorizontal, mdiArrowLeftCircleOutline, mdiArrowRightCircleOutline, mdiShuffle } from "@mdi/js";
 
 // example of a set
 // {
@@ -13,6 +15,33 @@ import { mdiArrowLeftCircleOutline, mdiArrowRightCircleOutline, mdiShuffle } fro
 // }
 
 export default function Review({ set, setId, classId }) {
+	const [index, setIndex] = useState(0);
+	const [isShowingFront, setIsShowingFront] = useState(true);
+	const numTerms = set.Terms.length;
+
+	const currentTerm = set.Terms[index];
+	const currentDef = currentTerm.Flashcards[0];
+
+	const frontSide = currentTerm.Content;
+	const backSide = currentDef.Content;
+
+	const flipCard = () => {
+		// setIsShowingFront((prevIsShowingFront) => {
+		// 	setIsShowingFront(!prevIsShowingFront)
+		// })
+		setIsShowingFront(!isShowingFront);
+	}
+
+	const toNextCard = () => {
+		setIndex(index + 1);
+		setIsShowingFront(true);
+	}
+
+	const toPrevCard = () => {
+		setIndex(index - 1);
+		setIsShowingFront(true);
+	}
+	
 	return (
 		<div className="review-page-container h-100 p-3">
 			<button className="btn btn-primary" type="button">
@@ -20,24 +49,26 @@ export default function Review({ set, setId, classId }) {
 			</button>
 			<div className="review-container mt-3 gap-3 d-flex flex-column align-items-center">
 				<h3 className="m-0">{set.Name} </h3>
-				<p className="">1/50</p>
+				<p className="">{index + 1}/{numTerms}</p>
 
-				<div className={`${styles.card} card p-3 justify-content-center align-items-center`}>
-					{/* only wanna see one card */}
+				<div className={`${styles.card} card p-3 justify-content-center align-items-center`} onClick={flipCard}>
+					{isShowingFront ? frontSide : backSide}
 
-					{set.Terms.map((term) => (
+					{/* {set.Terms.map((term) => (
 						<p className="m-0" key={`term-${term.Term_Id}`}>{term.Content}</p>
-					))}
+					))} */}
 				</div>
 
 				<div className={`${styles.reviewButtons} d-flex justify-content-between`}>
-					<button className={`${styles.optionsButton}`}>Options</button>
+					<button>
+						<Icon path={mdiDotsHorizontal} size={1.75}/>
+					</button>
 
 					<div className="">
-						<button className={`p-0`}>
+						<button className={`btn ${index === 0 ? 'disabled' : ''} p-0`} onClick={toPrevCard}>
 							<Icon path={mdiArrowLeftCircleOutline} size={2} color={`var(--light-blue-green)`}/>
 						</button>
-						<button className={`p-0`}>
+						<button className={`btn ${index === numTerms - 1 ? 'disabled' : ''} p-0`} onClick={toNextCard}>
 							<Icon path={mdiArrowRightCircleOutline} size={2} color={`var(--light-blue-green)`}/>
 						</button>
 					</div>
--- client/src/app/class/[classId]/[setId]/review/page.js
+++ client/src/app/class/[classId]/[setId]/review/page.js
@@ -4,11 +4,10 @@ import Review from "./Review";
 const apiUrl = process.env.API_URL;
 
 export default async function Index({ params }) {
-	const headersList = headers();
 	const classId = params.classId;
 	const setId = params.setId;
 
-	const response = await fetch(`${apiUrl}/api/set/${setId}`);
+	const response = await fetch(`${apiUrl}/api/set/${setId}`, { cache: 'no-store' });
 	const set = await response.json();
 
 	return (
