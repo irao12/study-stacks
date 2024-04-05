6a40f665e32ffb748fea08345489c94f5e78353c
ivanrao <ivanrao@outlook.com>
2024-04-04 22:03:36 -0400

    removed font awesome

--- client/src/app/class/page.js
+++ client/src/app/class/page.js
@@ -3,10 +3,6 @@ import ClassPage from "./components/ClassPage";
 export default function Index() {
 	return (
 		<main id="class-page">
-			<link
-				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-				rel="stylesheet"
-			/>
 			<div
 				className={`w-100 h-100 d-flex justify-content-center align-items-center`}
 			>
