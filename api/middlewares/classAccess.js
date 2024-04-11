ddb439c3946af2db7c222e2b5eed3281d1f7b672
ivanrao <ivanrao@outlook.com>
2024-04-11 19:27:45 -0400

    added middleware for checking class access

---
+++ api/middlewares/classAccess.js
@@ -0,0 +1,32 @@
+const {
+	Class,
+	ClassAccess,
+	Set,
+	Term,
+	User,
+	Flashcard,
+	Summary,
+} = require("../models");
+
+const checkClassAccess = async (req, res, next) => {
+	const user = req.user;
+
+	if (!user)
+		return res.status(401).json({ message: "User is not authenticated" });
+	let classId = req.params.classId || req.body.classId; // Check both URL params and request body for classId
+
+	// If classId is not found in URL params or request body, handle accordingly
+	if (!classId) {
+		return res.status(400).json({ message: "Class ID is missing" });
+	}
+
+	const classToAccess = await Class.findByPk(classId);
+
+	if (!classToAccess.hasUser(user)) {
+		return res.status(403).json({ message: "User does not have access" });
+	}
+
+	next();
+};
+
+module.exports = checkClassAccess;
