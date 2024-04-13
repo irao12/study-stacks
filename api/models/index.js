8b6320c294f2e40657b3a7354fbb39d2b71ecd08
ivanrao <ivanrao@outlook.com>
2024-04-13 00:55:31 -0400

    Changed the sequelize initalization to prepare for deployment

--- api/models/index.js
+++ api/models/index.js
@@ -9,8 +9,21 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-	sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.NODE_ENV == "production") {
+	sequelize = new Sequelize(
+		process.env.DB_DATABASE,
+		process.env.DB_USER,
+		process.env.DB_PASS,
+		{
+			host: process.env.DB_HOST,
+			dialect: "mysql",
+			dialectOptions: {
+				ssl: {
+					rejectUnauthorized: false,
+				},
+			},
+		}
+	);
 } else {
 	sequelize = new Sequelize(
 		config.database,
