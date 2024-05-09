dc1b0ea0c64a76a98d58fdd9c6008a7b84a30517
Stefani Hermanto <sc.shher@gmail.com>
2024-05-08 22:13:28 -0400

    Added logo to navbar

--- client/src/app/components/Navbar.js
+++ client/src/app/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./navbar.module.css";
 import LogOutButton from "./LogOutButton";
+import logo from "../../assets/Study_Stacks_Logo.PNG";
 
 export default function Navbar({ user }) {
 	return (
@@ -9,7 +11,13 @@ export default function Navbar({ user }) {
 			className={`${styles.navSection} navbar navbar-light justify-content-between p-3`}
 		>
 			<Link href="/">
-				<h4 className="m-0">StudyStacks</h4>
+				<div className="d-flex align-items-center gap-3">
+					<Image
+						src={logo}
+						className={`${styles.logo}`}
+					/>
+					<h4 className="m-0 flex-grow-0">StudyStacks</h4>
+				</div>
 			</Link>
 			<div className="nav-buttons d-flex gap-3">
 				{user !== null && <Link href="/class">Classes</Link>}
